53a4d0ce5c5b06b64a0dca1b4617e3b163a3e35d
DanlokPlay <93839221+DanlokPlay@users.noreply.github.com>
2024-03-24 16:51:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,126 +1,97 @@
 import telebot
-
 from datetime import datetime
+import time
+import requests
+from random import randint
+
+token = '6064246639:AAE-9Np2IM6rSAXlCOoxql6rgeLNd1QKcZI'
+bot = telebot.TeleBot(token)
 
 
-bot = telebot.TeleBot('6064246639:AAE-9Np2IM6rSAXlCOoxql6rgeLNd1QKcZI')
+def set_webhook_with_delay():
+    time.sleep(1)
+    bot.set_webhook()
 
-"""
-# DanlokPlay
-CHAT_ID = 1375077159
 
-# KatomanD
-CHAT_ID = -1001969870941
+API_URL = "https://api.telegram.org/bot{}/sendMessage".format(token)
 
-# Pechka TV
-CHAT_ID = 
+print('OK')
 
-# message.chat.id - во всех чатах. -> CHAT_ID - определённый чат
+def send_message_with_retry(chat_id, text):
+    retries = 3
+    delay = 1  # Initial delay in seconds
 
-"""
-# Реагирует на сообщение ( это у нас 1), а не на команду (начинается на /)
-"""
-@bot.message_handler(func = lambda message: message.text == '1')
-def start(message):
-    bot.send_message(message.chat.id, '2!')
-"""
-#Отправка в чат (определённый) (с упоминанием) send_message((CHAT_ID) message.chat.id, сообщение, (ответ на сообщение) reply_to_message_id = message.id)
+    for _ in range(retries):
+        response = requests.post(API_URL, data={"chat_id": chat_id, "text": text})
+        if response.status_code == 200:
+            return
+        elif response.status_code == 429:
+            print("Rate limit exceeded. Retrying after {} seconds.".format(delay))
+            time.sleep(delay)
+            delay *= 2  # Exponential backoff
+        else:
+            # Handle other types of errors
+            print("Unexpected error:", response.status_code)
+            return
 
-@bot.message_handler(commands=['код', 'к', 'пароль', 'k', 'kod'])
-def start(message):
+    print("Max retries exceeded. Unable to send message.")
+
+
+def read_codes_from_file(filename):
+    codes = {}
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                day, code = line.strip().split(':')
+                codes[int(day)] = code
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    except ValueError:
+        print(f"Invalid data format in '{filename}'.")
+    
+    return codes
+
+codes = read_codes_from_file('codes.txt')
+
+
+@bot.message_handler(commands=['код', 'к', 'пароль', 'k', 'kod', 'code', 'password'])
+def start_code(message):
     current_datetime = datetime.now()
     current_datetime_DAY = current_datetime.day
-    '''
-    if (current_datetime_DAY == 1 or current_datetime_DAY == 2):
-        bot.send_message(message.chat.id, '12')
-    if (current_datetime_DAY == 3 or current_datetime_DAY == 4):
-        bot.send_message(message.chat.id, '34')
-    if (current_datetime_DAY == 5 or current_datetime_DAY == 6):
-        bot.send_message(message.chat.id, '56')
-    if (current_datetime_DAY == 7 or current_datetime_DAY == 8):
-        bot.send_message(message.chat.id, '78')
-    if (current_datetime_DAY == 9 or current_datetime_DAY == 10):
-        bot.send_message(message.chat.id, '910')
-    if (current_datetime_DAY == 11 or current_datetime_DAY == 12):
-        bot.send_message(message.chat.id, '1112')
-    if (current_datetime_DAY == 12 or current_datetime_DAY == 14):
-        bot.send_message(message.chat.id, '1314')
-    if (current_datetime_DAY == 15 or current_datetime_DAY == 16):
-        bot.send_message(message.chat.id, '1516')
-    if (current_datetime_DAY == 17 or current_datetime_DAY == 18):
-        bot.send_message(message.chat.id, '1718')
-    if (current_datetime_DAY == 19 or current_datetime_DAY == 20):
-        bot.send_message(message.chat.id, '1920', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 21 or current_datetime_DAY == 22):
-        bot.send_message(message.chat.id, '2122')
-    if (current_datetime_DAY == 23 or current_datetime_DAY == 24):
-        bot.send_message(message.chat.id, '2324')
-    if (current_datetime_DAY == 25 or current_datetime_DAY == 26):
-        bot.send_message(message.chat.id, '2526')
-    if (current_datetime_DAY == 27 or current_datetime_DAY == 28):
-        bot.send_message(message.chat.id, '2728')
-    if (current_datetime_DAY == 29 or current_datetime_DAY == 30):
-        bot.send_message(message.chat.id, '2930')
-    if (current_datetime_DAY == 31):
-        bot.send_message(message.chat.id, '2930')
-    '''
-    if (current_datetime_DAY == 1):
-        bot.send_message(message.chat.id, '79230', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 2 or current_datetime_DAY == 3):
-        bot.send_message(message.chat.id, '96148', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 4 or current_datetime_DAY == 5):
-        bot.send_message(message.chat.id, '65806', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 6 or current_datetime_DAY == 7):
-        bot.send_message(message.chat.id, '52021', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 8 or current_datetime_DAY == 9):
-        bot.send_message(message.chat.id, '21815', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 10 or current_datetime_DAY == 11):
-        bot.send_message(message.chat.id, '18336', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 12 or current_datetime_DAY == 13):
-        bot.send_message(message.chat.id, '80418', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 14 or current_datetime_DAY == 15):
-        bot.send_message(message.chat.id, '08007', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 16 or current_datetime_DAY == 17):
-        bot.send_message(message.chat.id, '83286', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 18 or current_datetime_DAY == 19):
-        bot.send_message(message.chat.id, '34164', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 20 or current_datetime_DAY == 21):
-        bot.send_message(message.chat.id, '40314', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 22 or current_datetime_DAY == 23):
-        bot.send_message(message.chat.id, '02157', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 24 or current_datetime_DAY == 25):
-        bot.send_message(message.chat.id, '21066', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 26 or current_datetime_DAY == 27):
-        bot.send_message(message.chat.id, '13888', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 28 or current_datetime_DAY == 29):
-        bot.send_message(message.chat.id, '31673', reply_to_message_id = message.id)
-    if (current_datetime_DAY == 30 or current_datetime_DAY == 31):
-        bot.send_message(message.chat.id, '10162', reply_to_message_id = message.id)
+
+    if current_datetime_DAY in codes:
+        send_message_with_retry(message.chat.id, codes[current_datetime_DAY])
+
+    send_message_with_retry(1375077159, 'Кто-то посмотрел код на сегодня (!код)')
+    print("Пароль на сегодня выслан!")
+
 
 @bot.message_handler(commands=['коды', 'коды_на_месяц', 'месяц', 'бункер', 'бункер_альфа'])
-def start(message):
-    file = open('photo.jpg', 'rb')
-    bot.send_photo(message.chat.id, file, 'Коды на этот месяц')
-"""
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Привет!')
+def start_codes(message):
+    try:
+        with open('photo.jpg', 'rb') as file:
+            bot.send_photo(message.chat.id, file, 'Коды на этот месяц')
+    except FileNotFoundError:
+        print("Image file 'photo.jpg' not found.")
+    
+    send_message_with_retry(1375077159, 'Кто-то посмотрел коды на месяц (!коды)')
+    print("Пароли на месяцы высланы!")
 
-@bot.message_handler(commands=['прив'])
-def start(message):
-    bot.reply_to(message, 'Привет!')
+@bot.message_handler(commands=['м', 'монета'])
+def start_coins(message):
+    var = randint(0, 1)
+    if var == 0:
+        bot.send_message(message.chat.id, 'Орёл', reply_to_message_id=message.id)
+        send_message_with_retry(1375077159, 'Кто-то подбросил монету и выпал Орёл! (!монета)')
+    else:
+        bot.send_message(message.chat.id, 'Решка', reply_to_message_id=message.id)
+        send_message_with_retry(1375077159, 'Кто-то подбросил монету и выпала Решка! (!монета)')
+    print("Подброс монеты сработал!")
 
-@bot.message_handler(commands=['end'])
-def start(message):
-    bot.send_message(message.chat.id, 'Пока!')
-"""
-@bot.message_handler(commands=['chat_id'])
-def start(message):
-    bot.send_message(message.chat.id, message.chat.id)
+@bot.message_handler(commands=['info', 'инфо'])
+def start_info(message):
+    bot.send_message(message.chat.id, '/код(к, пароль, k, kode, code, password) - код на сегодня от БА \n/коды - коды на текущий месяц \n/монета(м) - подросить монету (Орёл или Решка) P.S. для важных решений:D ', reply_to_message_id=message.id)
+    bot.send_message(1375077159, 'Кто-то запросил список команд (!инфо)')
+    print("Информация о командах выслана!")
 
-@bot.message_handler(commands=['my_nick'])
-def start(message):
-    bot.send_message(message.chat.id, message.from_user.username)
-
-
-bot.polling()
+bot.infinity_polling()
